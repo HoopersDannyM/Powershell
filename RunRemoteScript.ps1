53cd4d6aa54896d269860c58532e8c92c45201da
HoopersDannyM <147047941+HoopersDannyM@users.noreply.github.com>
2024-08-19 13:52:21 +0100

    Add files via upload
    This PowerShell script is designed to execute a script or a series of commands on a remote machine. It allows administrators to manage multiple machines by deploying scripts remotely, automating tasks, and gathering outputs from remote systems. This script may leverage PowerShell Remoting, ensuring secure and efficient execution on remote devices.

---
+++ RunRemoteScript.ps1
@@ -0,0 +1,17 @@
+﻿# Parameters
+param (
+    [string]$remoteComputer = "VM NAME / PC NAME EG. LAPTOP12.DANNYS.LAN",
+    [string]$scriptPath = "C:\Users\dannym\Documents\Powershell Scripts\EventLogReport.ps1"
+)
+
+# Enable Remoting on Local Machine
+Enable-PSRemoting -Force
+
+# Configure Trusted Hosts
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value $remoteComputer -Force
+
+# Get Credentials
+$credential = Get-Credential
+
+# Run Script on Remote Computer
+Invoke-Command -ComputerName $remoteComputer -FilePath $scriptPath -Credential $credential
